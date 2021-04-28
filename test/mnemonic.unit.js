2233512a87405f32ad4023f2a30f59e44026ee45
arubinofaux <arubinofaux@gmail.com>
2021-04-28 12:56:47 -0600

    wip

--- test/mnemonic.unit.js
+++ test/mnemonic.unit.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var should = chai.should();
 
 var Mnemonic = require('..');
-var errors = require('bitcore-lib').errors;
+var errors = require('bitcore-doge-lib').errors;
 var bip39_vectors = require('./data/fixtures.json');
 const unorm = require('unorm');
 
